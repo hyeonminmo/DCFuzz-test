ef2361fd1ef3263bd240a59a220d0a6709bc82db
fuzz <hyeonminmo@hanyang.ac.kr>
2025-07-08 19:13:37 +0900

    fix cil package

--- docker/benchmark/dafl/setup_sparrow.sh
+++ docker/benchmark/dafl/setup_sparrow.sh
@@ -12,11 +12,10 @@ apt-get update
 apt-get install -y opam libclang-cpp12-dev libgmp-dev libclang-12-dev llvm-12-dev libmpfr-dev
 
 sed -i '/^opam init/ s/$/ --disable-sandboxing/' build.sh
+sed -i 's/opam install apron clangml/opam install conf-libclang.12 apron clangml/' build.sh
+sed -i 's|opam pin add cil https://github.com/prosyslab/cil.git -n|opam pin add cil https://github.com/prosyslab/cil.git#8e87fe45 -n|' build.sh
 ./build.sh
 
-#echo "***********************************************************************************************"
-#exit 0
-#opam install cli claml ppx_compare yojson ocamlgraph memtrace lymp clangml conf-libclang.12 batteries apron conf-mpfr linenoise
 opam install ppx_compare yojson ocamlgraph memtrace lymp clangml conf-libclang.12 batteries apron conf-mpfr cil linenoise claml
 
 eval $(opam env)
